40b1cb078d191c4dcf4c1916193eca35a601ed48
democraz20 <mozartrasig@gmail.com>
2022-04-03 11:56:36 +0700

    Update externalPrograms.cs
    checks if files in externalPrograms.cs exists first
    also added a bunch of C:\windows programs
    still needs to update main.cs to call these

--- src/externalPrograms.cs
+++ src/externalPrograms.cs
@@ -13,10 +13,33 @@ namespace ROCKET{
                 case "file":
                     Programs = @"C:\windows\explorer.exe";
                     break;
-                
+                case "rtf":
+                    Programs = @"C:\windows\write.exe";
+                    break;
+                case "cmd":
+                    Programs = @"C:\windows\system32\cmd.exe";
+                    break;
+                case "py":
+                    Programs = @"C:\python\py.exe";
+                    break;
+                case "calc":
+                    Programs = @"C:\windows\system32\calc.exe";
+                    break;
+                case "powershell":
+                    Programs = @"C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe";
+                    break;
+                case "winver":
+                    Programs = @"C:\windows\system32\winver.exe";
+                    break;
             }
-            FileInfo file = new FileInfo(Programs);
-            new Process { StartInfo = new ProcessStartInfo(Programs) { UseShellExecute = true } }.Start();
+            //check if file exists
+            if(File.Exists(Programs)){
+                FileInfo file = new FileInfo(Programs);
+                new Process { StartInfo = new ProcessStartInfo(Programs) { UseShellExecute = true } }.Start();
+            }
+            else{
+                Console.WriteLine("File not found");
+            }
         }
     }
 }
